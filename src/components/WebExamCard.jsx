c0971f97ba0d2e59628c8165288ebb8a0cfd68ee
Norihiko Ota <norihikoota77@gmail.com>
2026-06-09 11:17:42 +0900

    update WebExamCard

--- src/components/WebExamCard.jsx
+++ src/components/WebExamCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
